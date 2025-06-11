6a703d0c9bb78e80a943d035baedae177c965ef6
darrenchrist30 <c14220119@john.petra.ac.id>
2025-06-11 18:29:40 +0700

    update player_stats

--- db.php
+++ db.php
@@ -6,8 +6,8 @@ $databaseName = "nba_projek";
 
 try {
     // $client = new MongoDB\Client();
-    // $client = new MongoDB\Client("mongodb://localhost:27017");
-    $client = new MongoDB\Client();
+    $client = new MongoDB\Client("mongodb://mongo:mongo@localhost:27017");
+    // $client = new MongoDB\Client();
     $db = $client->selectDatabase($databaseName);
 
     // Koleksi yang akan kita gunakan:
